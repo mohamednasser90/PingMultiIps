fde1c411709d52e83181f1c2ab269a22b72d4733
mohamednasser90 <38047559+mohamednasser90@users.noreply.github.com>
2022-07-17 09:16:08 +0300

    add script PS

---
+++ CheckIPsConnection.ps1
@@ -0,0 +1,14 @@
+﻿$names=Get-content "G:\CheckingPoint\PS\listOfIps.txt"
+foreach($name in $names){
+if(Test-Connection -ComputerName $name -Count 1 -ErrorAction SilentlyContinue){
+Write-Host "$name is up" -ForegroundColor Green
+$output+="$name is up,"+"`n"
+}
+else{
+Write-Host "$name is down" -ForegroundColor Red
+$output+="$name is down,"+"`n"
+}
+
+}
+$output | Out-File "G:\CheckingPoint\PS\result.txt"
+Start-Sleep -s 15
